6005cdc5e827f9b45333d226fc0da50e2de2a557
sebschauer <sebschauer@sebschauer.de>
2024-07-07 14:49:24 +0200

    wip

---
+++ XdtClassGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace XdtClassGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
